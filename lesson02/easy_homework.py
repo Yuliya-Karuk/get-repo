a353c72628a6a2f4ae98ff24ede9633163168296
Yuliya-Karuk <karuk.yuliya@gmail.com>
2019-03-22 00:00:33 +0300

    Написано 2 решения для второй задачи из раздела easy из второго урока

--- lesson02/easy_homework.py
+++ lesson02/easy_homework.py
@@ -19,3 +19,37 @@ n = 1
 for x in fruits:
     print(n, " {:>10}".format(x))
     n += 1
+
+# Задача-2:
+# Даны два произвольные списка.
+# Удалите из первого списка элементы, присутствующие во втором списке.
+
+fruits = ["яблоко", "банан", "киви", "арбуз", "клубника", "слива", "черешня", "инжир", "хурма"]
+exotic_fruit = ["инжир", "маракуйа", "ананас", "хурма"]
+print(fruits)
+print(exotic_fruit)
+
+for fruit in exotic_fruit:
+    if fruit in fruits:
+        fruits.remove(fruit)
+
+print(fruits)
+print(exotic_fruit)
+
+fruits = ["яблоко", "банан", "киви", "арбуз", "клубника", "слива", "черешня", "инжир", "хурма"]
+exotic_fruit = ["инжир", "маракуйа", "ананас", "хурма"]
+print(fruits)
+print(exotic_fruit)
+new = []
+
+for item in set(fruits).difference(exotic_fruit):
+    new.append(item)
+
+fruits = new
+print(fruits)
+print(exotic_fruit)
+
+
+
+
+
