84efbb4f731c5bb018a67ebe16e7f0a2e34601c2
Yuliya-Karuk <karuk.yuliya@gmail.com>
2019-03-29 00:06:14 +0300

    Написано решение 3 задачи раздела hard 3 урока

--- lesson03/hard_homework.py
+++ lesson03/hard_homework.py
@@ -117,7 +117,6 @@ file_hour.seek(0)
 
 def reading(file, n):
     new = []
-    d = {}
     for i in range(n):
         str = file.readline().split()
         new.append(str)
@@ -149,6 +148,32 @@ for i in lst1:
 file_worker.close
 file_hour.close
 
+# Задание-3:
+# Дан файл ("data/fruits") со списком фруктов.
+# Записать в новые файлы все фрукты, начинающиеся с определенной буквы.
+# Т.е. в одном файле будут все фрукты на букву “А”, во втором на “Б” и т.д.
+# Файлы назвать соответственно.
+# Пример имен файлов: fruits_А, fruits_Б, fruits_В ….
+# Важно! Обратите внимание, что нет фруктов, начинающихся с некоторых букв.
+# Напишите универсальный код, который будет работать с любым списком фруктов
+# и распределять по файлам в зависимости от первых букв, имеющихся в списке фруктов.
+# Подсказка:
+# Чтобы получить список больших букв русского алфавита:
+# print(list(map(chr, range(ord('А'), ord('Я')+1))))
 
+f = open("fruits.txt", "r")
+alphabet = list(map(chr, range(ord('А'), ord('Я')+1)))
+
+def new_file(f):
+    for line in f:
+        for j in alphabet:   # считываем файл построчно
+            name = 'fruits_' + j + '.txt'
+            if j in line:                # пока не найдем нужную информацию
+                file = open(name, 'a')
+                file.write(line)
+            else:
+                pass
+
+new_file(f)
 
 
