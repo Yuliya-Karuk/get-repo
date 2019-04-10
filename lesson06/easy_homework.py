ad4b33b686649c72abd6a49763e451d88a27db5b
Yuliya-Karuk <karuk.yuliya@gmail.com>
2019-04-10 23:05:46 +0300

    Упрощена запись координат 1 задания раздела easy 6 урока

--- lesson06/easy_homework.py
+++ lesson06/easy_homework.py
@@ -4,13 +4,9 @@
 import math
 
 class Triangle:
-    def __init__(self, x1, x2, x3, y1, y2, y3):
-        self.x1 = x1
-        self.x2 = x2
-        self.x3 = x3
-        self.y1 = y1
-        self.y2 = y2
-        self.y3 = y3
+    def __init__(self, points):
+        # points[[x1, y1], [x2, y2], [x3, y3]] координаты треугольника, a, b, c - стороны, s - площадь, h - высота
+        self.points = points
         self.a = 0
         self.b = 0
         self.c = 0
@@ -19,26 +15,39 @@ class Triangle:
 
 
     def square(self):
-        self.s = math.fabs(((self.x2 - self.x1)*(self.y3 - self.y1) - (self.x3 - self.x1)*(self.y2 - self.y1))/2)
+        # вычисление площади треугольника
+        self.s = math.fabs(((self.points[1][0] - self.points[0][0])*(self.points[2][1] - self.points[0][1]) - (self.points[2][0] - self.points[0][0])*(self.points[1][1] - self.points[0][1]))/2)
         return self.s
 
     def perimeter(self):
-        points = [[self.x1, self.y1], [self.x2, self.y2], [self.x3, self.y3]]
-        self.a = int(math.fabs(math.sqrt((points[1][0] - points[0][0]) ** 2 + (points[1][1] - points[0][1]) ** 2)))
-        self.b = int(math.fabs(math.sqrt((points[2][0] - points[1][0]) ** 2 + (points[2][1] - points[1][1]) ** 2)))
-        self.c = int(math.fabs(math.sqrt((points[2][0] - points[0][0]) ** 2 + (points[2][1] - points[0][1]) ** 2)))
+        # вычисление сторон треугольника
+        self.a = int(math.fabs(math.sqrt((self.points[1][0] - self.points[0][0]) ** 2 + (self.points[1][1] - self.points[0][1]) ** 2)))
+        self.b = int(math.fabs(math.sqrt((self.points[2][0] - self.points[1][0]) ** 2 + (self.points[2][1] - self.points[1][1]) ** 2)))
+        self.c = int(math.fabs(math.sqrt((self.points[2][0] - self.points[0][0]) ** 2 + (self.points[2][1] - self.points[0][1]) ** 2)))
         summ = self.a + self.b + self.c
         return summ
 
     def height(self):
+        # вычисление высоты треугольника
         self.h = int(self.s * 2 / self.a)
         return self.h
 
-
-trian_1 = Triangle(-1, 3, 5, -3, 4, -5)
+points = [[-1, -3], [3, 4], [5, -5]]
+trian_1 = Triangle(points)
 print(f"Площадь треугольника равна: {trian_1.square()}")
 print(f"Периметр треугольника равен: {trian_1.perimeter()}")
 print(f"Высота треугольника равна: {trian_1.height()}")
+
+
+# Задача-2: Написать Класс "Равнобочная трапеция", заданной координатами 4-х точек.
+# Предусмотреть в классе методы:
+# проверка, является ли фигура равнобочной трапецией;
+# вычисления: длины сторон, периметр, площадь.
+
+
+
+
+
 
 
 
